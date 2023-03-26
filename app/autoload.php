f4a6d190c069a2aa600fcb20a71fbf0b806f4ac1
AlexBreno <105872871+DevAlexBreno@users.noreply.github.com>
2023-03-26 12:44:51 -0300

    Adicionando o arquivo spl_autoload

---
+++ app/autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+function autoload($class_Name){
+
+    $dirs = array("controller/","model/","view/");
+
+    foreach($dirs as $dir){
+
+        $file = ("$dir" . "$class_Name" . ".php");
+
+        if(file_exists($file)){
+        
+            include_once($file);
+        
+        }
+    }
+}
+
+spl_autoload_register('autoload');
+
+?>
