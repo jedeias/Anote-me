7afe3041e56a4491d979bea6fa6c0fd8c6065f7f
AlexBreno <105872871+DevAlexBreno@users.noreply.github.com>
2023-04-02 12:50:57 -0300

    Merge branch 'master' of https://github.com/jedeias/tcc
    Co-authored-by: Jedeias Luiz <jedeias@users.noreply.github.com>

---
+++ app/model/insert.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once("connect.php");
+
+
+class Insert
+{
+    private $conn;
+
+
+    public function __construct()
+    {
+        $this->conn = new Connect();
+    }
+
+    public function insert_activities()
+    {
+        $stmt = $this->conn->prepare("INSERT INTO atividade (pk_atividade) VALUES (NULL)");
+
+        $stmt->execute();
+    }
+
+    public function insert_type_activites($assunto, $descricao)
+    {
+        $atividade = new Insert();
+        $atividade->insert_activities();
+        
+        $stmt = $this->conn->prepare("INSERT INTO tipo_atividade (pk_tipo_atividade, fk_atividade, finalidade, descricao) VALUES (default, '$atividade', '$assunto', '$descricao')"); 
+ 
+        $stmt->execute();
+    }
+}
+
+?>
