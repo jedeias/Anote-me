b07ed7abb491d316df1d553ba3dc702f405bce5b
Júlia <juliarodriguesgoncalves31@gmail.com>
2023-04-06 20:29:59 -0300

    Acrescentando os metodos especias

--- app/config/host.php
+++ app/config/host.php
@@ -1,11 +1,21 @@
 <?php
 
+// Pressetar metodos especiais
+
+
+
+
 class Host{
 
     public $server = "localhost";
     public $database = "clinica_psicologica";
     public $user = "root";
     public $password = "";
+
+    public function getServer()
+    {
+        return $this->server;
+    }
 
 }
 
