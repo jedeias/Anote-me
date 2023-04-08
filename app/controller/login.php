f6ad8a3244e7eb74a2af5a5ec56ea46637cfbbf2
jedeias_Luiz <jedeiasluiz3@gmail.com>
2023-04-08 17:57:42 -0300

    Iniciada a conexão do backend com o frontend da página do paciente.
    Co-authored-by: Luan Henrique <Nibelzin@users.noreply.github.com>

--- app/controller/login.php
+++ app/controller/login.php
@@ -4,14 +4,12 @@
 
 class Login
 {
-    public function login_check($email, $password)
-    {
+    public function login_check($email, $password){
         $select = new Select();
         return $result = $select->validateUser($email, $password);
     }
 
-    public function user_data($email, $password)
-    {
+    public function user_data($email, $password){
         $select = new Select();
         
         return $result = $select->userData($email, $password); 
--- app/model/Crud.php
+++ app/model/Crud.php
@@ -1,26 +1,20 @@
 <?php
 
-class Crud
-{
-    private $conn;
-
-
-    public function __construct()
-    {
-        $this->conn = new Connect();
-    }
+class Crud extends Connection{
 
     public function insert_type_activites($assunto, $descricao)
     {
-        $stmt = $this->conn->prepare("INSERT INTO tipo_atividade (pk_tipo_atividade, finalidade, descricao) VALUES (default, '$assunto', '$descricao')"); 
+        $stmt = $this->prepare("INSERT INTO tipo_atividade (pk_tipo_atividade, finalidade, descricao) VALUES (default, '$assunto', '$descricao')"); 
         $stmt->execute();
    
     }
 
     public function insert_activitie(){
         
-        $sql = $this->conn->prepare("SELECT max(pk_tipo_atividade) as last_id FROM tipo_atividade");
+        $sql = $this->prepare("SELECT max(pk_tipo_atividade) as last_id FROM tipo_atividade");
+        
         if ($sql->execute()) 
+        
         {
 
             $result = $sql->get_result()->fetch_assoc();
@@ -28,7 +22,7 @@ class Crud
 
             if (isset($result) == 1) 
             {
-                $stmt = $this->conn->prepare("INSERT INTO atividade (pk_atividade, fk_tipo_atividade) VALUES (default,'$last_id')");
+                $stmt = $this->prepare("INSERT INTO atividade (pk_atividade, fk_tipo_atividade) VALUES (default,'$last_id')");
                 if ($stmt !== false) 
                 {
                     $stmt->execute();
@@ -42,6 +36,11 @@ class Crud
             echo "not execute first if";
         }
     }
+
+
+
+    
+    
 }
 
 ?>
