d731b344a8b18cdb2231e5afcdc3891955ce012e
Luan <Live12luan@hotmail.com>
2023-04-12 12:16:49 -0300

    data e hora fixed

--- app/view/telas/psicologo/psiPacientes.php
+++ app/view/telas/psicologo/psiPacientes.php
@@ -28,11 +28,6 @@ if (empty($_SESSION)) {
     <script src='../../JS/index.global.min.js'></script>
     <link rel="stylesheet" href="../../CSS/calendario.css">
     <link rel='stylesheet' href='../../CSS/psiPacientes.css'>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- Adicione os arquivos JavaScript do Bootstrap -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNSG+p" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 <body id='body'>
     <header class='header-container'>
@@ -129,8 +124,8 @@ if (empty($_SESSION)) {
 
                                     echo "<div class='activity-header'>";
 
-                                        echo "<p>" . $dado['data_hora'] . "</p>"; 
-                                        echo "<p>16/02/2023</p>";
+                                        echo "<p>" . $dado['hora'] . "</p>"; 
+                                        echo "<p>". $dado['data']. "</p>";
 
                                     echo "</div>";
 
