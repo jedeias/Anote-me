7e0d962edf2d71f68c4270b21761dcbbd5f67a6c
jedeias_Luiz <jedeiasluiz3@gmail.com>
2023-04-14 15:24:21 -0300

    Atualizei a interface CRUD.

--- app/controller/crud.php
+++ app/controller/crud.php
@@ -4,9 +4,10 @@
 
         public function insert_notas_paciente($id, $emocao, $fk_psicologo, $descricao);
         public function insert_atividades_paciente($fk_psicologo, $fk_paciente, $assunto, $atividade);
-        public function atualizar_perfil_psicologo($nome, $email, $senha, $id, $imagem);
+        public function atualizar_perfil_psicologo($nome, $email, $senha, $id);
         public function atualizar_perfil_paciente($nome, $email, $senha, $id);
         public function atualizar_perfil_secretario($nome, $email, $senha, $id);
+        
     }
 
 
